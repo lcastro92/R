ddc118e54715364cecb039a443241b3ed70cd5d3
Lucas Castro <lucas.castro.0307@gmail.com>
2021-11-01 11:03:33 -0300

    Agregado Variables

---
+++ variables.R
@@ -0,0 +1,19 @@
+#No hace falta una palabra reservada para declarar una variable
+#Se utiliza <- para guardr los datos en una variable
+#Las variables deben comenzar con una letra si o sí, es case-sensitive
+#Ejemplo:
+
+name <- "Lucas"
+num <- 29
+
+#para imprimir usamos print()
+
+print(name)
+print (num)
+#para ver el tipo de dato usamos class
+
+class(name)
+class(num)
+
+#Tambien puedo concatenar datos con paste
+paste("Mi nombre es ",name," tengo ",num)
